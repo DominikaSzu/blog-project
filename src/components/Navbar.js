ad33251930bebd962fea2a9904d03e444ece28e3
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-28 21:57:08 +0100

    Navbar modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,20 @@ import React from 'react';
 class Navbar extends React.Component {
 	render() {
 		return (
-			<p>Navbar</p>
+			<div className="navbar">
+				<div className="menu">
+				MENU
+				</div>
+				<div className="categories">
+					<div className="category about-me">O mnie</div>
+					<div className="category blog">Blog</div>
+					<div className="category beauty-events">Eventy Urodowe</div>
+					<div className="category makeup">Makijaż</div>
+					<div className="category makeup-bride">Makijaż Ślubny</div>
+					<div className="category fashion">Stylizacje</div>
+					<div className="category t-shirts">T-Shirty Górnicka Design</div>
+				</div>
+			</div>
 	)}
 }
 
