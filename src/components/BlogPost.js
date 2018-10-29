a0bc17c4959b82bd9f7d987ff7d9c0e1385f1082
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-29 14:58:21 +0100

    BlogPost img styled

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import dog from '../images/dog.jpg';
 
 class BlogPost extends React.Component {
 	render() {
 		return (
 			<article className="blog-post">
+				<img src={dog} alt="dog" className="blog-post-photo"/>
 				<h3 className="title">Danish sweet chocolate</h3>
 				<p className="post-content">
 				Gummi bears gingerbread wafer jelly beans icing tootsie roll fruitcake apple pie halvah. Cupcake sugar plum liquorice oat cake. Halvah bonbon dessert chocolate cake pastry brownie. Cake candy canes biscuit marzipan macaroon jujubes.	
