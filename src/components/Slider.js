e7e783b83a5dba3b5f6cb6ed58ecc86001e8a28a
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-31 14:14:44 +0100

    img changed

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import photo from '../images/gallery.jpg';
 
 class Slider extends React.Component {
 	render() {
 		return (
 			<div className="gallery">
 				<figure className="photo-box">
-					<img className="photo-img" src={photo} alt="Pink Book"/>
 				</figure>
 			</div>
 	)}
